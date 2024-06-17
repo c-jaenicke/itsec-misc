2b77f6ebdd7f5b05d85bd4e61cc87c67f8a0cf4a
c-jaenicke <72254270+c-jaenicke@users.noreply.github.com>
2024-06-17 17:23:35 +0200

    Update ports-prod.sh

--- scripts/ports-prod.sh
+++ scripts/ports-prod.sh
@@ -14,10 +14,10 @@ iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
 
 # allow dns traffic on port 53, both tcp and udp
-iptables -A INPUT -p tcp --dport 53 -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 53 -j ACCEPT
-iptables -A INPUT -p udp --dport 53 -j ACCEPT
-iptables -A OUTPUT -p udp --sport 53 -j ACCEPT
+iptables -A INPUT -p tcp --sport 53 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
+iptables -A INPUT -p udp --sport 53 -j ACCEPT
+iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 
 # allow ntp traffic on port 123
 iptables -A INPUT -p udp --dport 123 -j ACCEPT
@@ -28,8 +28,8 @@ iptables -A INPUT -p icmp -j  ACCEPT
 iptables -A OUTPUT -p icmp -j ACCEPT
 
 # allow http on port 80
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 80 -j ACCEPT
+iptables -A INPUT -p tcp --sport 80 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
 
 # allow https on port 443
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
